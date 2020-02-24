2fbeda291ec2cc6dbb37711731c97340df906344
steph-al <stephanie@sidium360.com>
2020-02-24 12:11:04 -0500

    Update stripe-widget.js

--- stripe-widget.js
+++ stripe-widget.js
@@ -3,9 +3,11 @@
   
   // Set your publishable key: remember to change this to your live publishable key in production
   // See your keys here: https://dashboard.stripe.com/account/apikeys
-  var stripe = Stripe('pk_test_TPooMQcsvdEDq54NiTphI7jx');
-  var stripe = Stripe('rk_test_Zu4Vz52eIYFNdGoFKe8aYiQn00gcclUvS0')
-  var stripe = Stripe('sk_test_NfRutFA9sgl5e1AUYzjwiNNt00UG6t6fvY')
+  var stripe = Stripe('pk_test_TPooMQcsvdEDq54AiTphI7jx');
+  var stripe = Stripe('rk_test_Zu4Vz52eIYFNdGoFKe9aYiQn00gcclUvS0')
+  var stripe = Stripe('sk_test_AfRutFA9sg45e1AUYzjwiNNt00UG6t6fvY')
+
+  var stripe = Stripe('sk_test_NfRutFA9sg45e1AUYzjwiNNt00UG6t6fvY')
   var elements = stripe.elements(); 
     var elements = stripe.elements({
       fonts: [
