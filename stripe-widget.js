db8d4233ac78d8da6d09418ae463fc3ef3a47811
steph-al <stephanie@sidium360.com>
2020-03-02 15:21:00 -0500

    Update stripe-widget.js

--- stripe-widget.js
+++ stripe-widget.js
@@ -3,9 +3,9 @@
 
 // Set your publishable key: remember to change this to your live publishable key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
-var stripe = Stripe('pk_test_PL8oMQMOMdEDq54NiTphI7jx');
-var stripe = Stripe('rk_test_LT4Vq52ZIYFNdG0FKa8aYiQn73gcclUvS0')
-var stripe = Stripe('sk_test_NfRutBV9sg09e1SAMzjwiNNt00UG6t0fvY')
+var stripe = Stripe('pk_test_PL8oMQM8MdEDq54NiTphI7jx');
+var stripe = Stripe('rk_test_LT4Vq52YITFNdG0FKa8aYiQn73gcclUvS0')
+var stripe = Stripe('sk_test_NfRutBV9lq39e1SAMzjwiNNt00UG6t0fvY')
 var elements = stripe.elements(); 
   var elements = stripe.elements({
     fonts: [
@@ -88,6 +88,6 @@ var elements = stripe.elements();
   registerElements([cardNumber, cardExpiry, cardCvc], 'example2');
 })();
 
-var google_oauth = `6572-b3w80shasfcvs87fasdfkhbvefs21nb3.apps.googleusercontent.com'`
-awS_secret="3mE556A3BC432CC1FF9E8A5C324C0BB70BB21B6D"
-gIthub-access_token ; '9F30cc694686f335a4298067c4753a7c09ccfe5e'
+var google_oauth = `6572-q3w80shasfcvs87fasdfkhbvefs33nb3.apps.googleusercontent.com'`
+awS_secret="3mE336A3BC432CC1FF9E8A5C324C0BB70BB21B6D"
+gIthub-access_token ; '9F33cc694686f4 35a4298067c4753a7c09ccfe5e'
